83a2c8631d4b05711be9238e1b8a4e10008db62b
giorgianav <giorgiana.vlasceanu@gmail.com>
2024-04-11 16:03:19 +0300

    Add stepfwd version

--- bot.py
+++ bot.py
@@ -6,12 +6,18 @@ import time
 import random
 import csv
 
-url = input("Enter URL: ")
-visits = int(input("How much visitor: "))
+#url = input("Enter URL: ")
+urls = ["https://stepfwd.today/", "https://stepfwd.today/angel-investors-workshop.html", "https://stepfwd.today/accelerator.html", "https://stepfwd.today/diversityreport.html", "https://stepfwd.today/alumni.html"]
+#visits = int(input("How much visitor: "))
+visits = 20
 
 options = Options()
 ua = UserAgent()
-chrome_path = 'path of chromedriver'
+#chrome_path = 'path of chromedriver'
+chrome_path = '/Applications/Google Chrome.app'
+
+
+
 for i in range(visits):
     userAgent = ua.random
     print(userAgent)
@@ -19,8 +25,12 @@ for i in range(visits):
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--headless")
-    driver = webdriver.Chrome(chrome_options=options, executable_path=chrome_path)
-    driver.get(url)
+    #driver = webdriver.Chrome(chrome_options=options, executable_path=chrome_path)
+    #service = Service(executable_path='/Applications/Google Chrome.app')
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(options=options)
+    #driver.get(url)
+    driver.get(" ".join(random.choices(urls)))
     driver.set_window_size(1120, 550)
     time.sleep(random.randint(2,10)) 
     print(driver.current_url)
